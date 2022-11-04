00716c4508289954d3acb9e267fbc0177ecd352e
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2022-11-04 22:08:27 +0700

    Change url route and change view argument of render function inside app.get()

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ app.set('view engine', 'ejs')
 
 //Serving static files
 app.use(express.static('static'))
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index', {
                             title: pagesName[0],
                             cssHref: pagesCss["Admin login"]
                         })
 })
-app.get('/', (req, res) => {
-    res.render('index', {
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard', {
                             title: pagesName[1],
                             cssHref: pagesCss["Dashboard"]
                         })
