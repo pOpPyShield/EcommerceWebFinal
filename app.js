2eb5fc45e20a801858f195fbed53e3287f838b42
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2022-11-06 10:22:56 +0700

    Code async promise to get value from query and direct user with match username and password to dashboard page

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "huyroot",
+    password: "Abcd1234",
+    database: "EcommerceWebSite"
+})
 const pagesCss = {"Admin login": "index.css",
                 "Dashboard": "index.css"}
 const pagesName = Object.keys(pagesCss)
@@ -9,6 +17,10 @@ app.set('view engine', 'ejs')
 
 //Serving static files
 app.use(express.static('static'))
+
+//Use body-parser to encoded message
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.render('index', {
                             title: pagesName[0],
@@ -22,6 +34,26 @@ app.get('/dashboard', (req, res) => {
                         })
 })
 
+app.post('/login-process', (req, res) => {
+    (async () => {
+        con.connect()
+        const result = await checkAdmin(req.body.userName, req.body.password)
+        con.end()
+        if (result === 1) {
+            res.redirect('/dashboard')
+        } else {
+            res.redirect('/')
+        }
+    })()
+})
+function checkAdmin(userName, password) {
+    let queryStatement = "SELECT COUNT(*) FROM `Admin` WHERE UserName='" + userName +"' and Password='" + password +"'"
+    return new Promise((resolve, reject) => {
+        con.query(queryStatement, (err, result) => {
+            return err ? reject(err) : resolve(result[0]['COUNT(*)'])
+        })
+    })
+}
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
