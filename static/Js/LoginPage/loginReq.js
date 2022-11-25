4896a062eeae26295bad8f74745849dac8c7dd88
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2022-11-25 21:58:18 +0700

    Fix absolute url to relative url

--- static/Js/LoginPage/loginReq.js
+++ static/Js/LoginPage/loginReq.js
@@ -2,7 +2,7 @@
             userName: document.querySelector("#username").value,
             password: document.querySelector("#password").value
             */
-var loginAPI = "http://localhost:3000/"
+var loginAPI = "/"
 function login(data) {
     var options  = {
         method: 'POST',
@@ -20,7 +20,7 @@ function login(data) {
                 document.querySelector("#password").value=""
                 window.location.reload()
             } else {
-                window.location.href="http://localhost:3000"+resp.redirect_path
+                window.location.href="/"+resp.redirect_path
             }
         })
         .catch((err) => {
