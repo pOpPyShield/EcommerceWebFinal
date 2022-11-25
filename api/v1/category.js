7ee55bdc6d76b25d0767b47f378da9f97a58eba2
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2022-11-25 11:00:38 +0700

    Delete category.js in api

--- api/v1/category.js
+++
@@ -1,9 +0,0 @@
-const { json } = require("body-parser")
-const con = require("../../database/db")
-function queryCategory() {
-    let queryStatement = "SELECT * FROM `Category`"
-    con.query(queryStatement, (err, data) => {
-        return err ? json({message: "Error executing"}) : json(data)
-    })
-}
-module.exports = {queryCategory, con}
