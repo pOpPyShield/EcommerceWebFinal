41237c5fcf976ff911c22980bb86a1a626872863
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2022-12-25 15:52:18 +0700

    Success add category to database

--- src/controllers/categorycontroller.js
+++ src/controllers/categorycontroller.js
@@ -1,9 +1,11 @@
 const Category = require("../models/")["Category"]
+const Gender = require("../models/")["Gender"]
 const CategoryService = require("../services/categoryservices")
 function insertCategory(req, res, next) {
     (async() => {
         try {
-            var categoryObj = await Category.create({name: req.body.name})
+            var gender = await Gender.findOne({where: {name: req.body.genName}})
+            var categoryObj = await Category.create({name: req.body.name, createdAt: new Date(), updatedAt: new Date(), GenderId: gender.id})
             await categoryObj.save()
             res.json({result: categoryObj.name, operation: "Add"})
         } catch(err) {
