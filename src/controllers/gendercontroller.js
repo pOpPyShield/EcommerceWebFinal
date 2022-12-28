b7cd95e1fdbf937143d27ec8294efdc9e44f66e4
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2022-12-28 21:53:20 +0700

    Success create add gender

--- src/controllers/gendercontroller.js
+++ src/controllers/gendercontroller.js
@@ -1,5 +1,13 @@
 const Gender = require("../models/")["Gender"]
+const ImageGender = require("../models/")["ImageGender"]
 const SecurityService = require("../services/securityservice")
+const pathSave = require('path').resolve(__dirname, '..')+"/Static/imgs/GenderImg/"
+function getNameImage(url) {
+    return url.split('.').slice(0, -1).join('.')
+}
+function getFileExtension(url) {
+    return url.split('.').pop()
+}
 function insertGender(req, res, next) {
     (async() => {
         try {
@@ -10,23 +18,20 @@ function insertGender(req, res, next) {
             if(!req.files || Object.keys(req.files).length ==0) {
                 throw new Error("File not upload successful")
             }
+            var fileName = getNameImage(req.files.myFile.name)
+            var fileType = getFileExtension(req.files.myFile.name)
+            var imageGender = await ImageGender.create({path: fileName, ext: fileType, GenderId: gender.id})
+            await imageGender.save()
+            var fileObj = req.files.myFile
+            fileObj.mv(pathSave+fileName+"."+fileType, (err) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    console.log("file uploaded")   
+                }
+            })
             res.json({result: gender.name, operation: "Add"})
         } catch(err) {
-            res.send(err)
-        }
-    })()
-}
-function uploadFile(req, res, next) {
-    (async() => {
-        try {
-            console.log(req)
-            if(!req.files || Object.keys(req.files).length == 0) {
-                throw new Error("File not upload successfull")
-            } 
-            console.log(req.files.myFile)
-            res.json({result: true})
-        } catch(err) {
-            console.log(err)
             res.send(err)
         }
     })()
